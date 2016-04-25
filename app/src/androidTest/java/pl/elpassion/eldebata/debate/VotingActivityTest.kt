6c528493a3225969229295126e41a611ec5e9d52
Dominik <dominik.malantowicz@gmail.com>
2016-04-25 22:38:03 +0200

    Voting Activity should have correct topic at the start if call return success

--- app/src/androidTest/java/pl/elpassion/eldebata/debate/VotingActivityTest.kt
+++ app/src/androidTest/java/pl/elpassion/eldebata/debate/VotingActivityTest.kt
@@ -2,15 +2,32 @@ package pl.elpassion.eldebata.debate
 
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito
+import pl.elpassion.eldebata.R
+import pl.elpassion.eldebata.common.hasText
+import pl.elpassion.eldebata.common.onId
 import pl.elpassion.eldebata.common.rule
+import pl.elpassion.eldebata.debate.api.*
+import rx.Observable
+import org.mockito.Mockito.`when` as on
 
 class VotingActivityTest {
 
-    @JvmField @Rule
-    val rule = rule<VotingActivity> {}
-    
-    @Test
-    fun initTest() {
+    val api = Mockito.mock(DebateApi::class.java)
+    val debateData = DebateData(topic = "Czy luzne podejscie do pracy grozi sukcesu firmy?", answers = Answers(
+            positive = Answer(1, "Grozi"),
+            negative = Answer(2, "Nie grozi"),
+            neutral = Answer(4, "Nie wiem")
+    ))
 
+    @JvmField @Rule
+    val rule = rule<VotingActivity> {
+        on(api.getDebateData()).thenReturn(Observable.just(debateData))
+        DebateApiProvider.override = api
+    }
+
+    @Test
+    fun shouldHaveCorrectTopicAtTheStart() {
+        onId(R.id.voting_activity_debate_topic).hasText(debateData.topic)
     }
 }
--- app/src/main/java/pl/elpassion/eldebata/debate/VotingActivity.kt
+++ app/src/main/java/pl/elpassion/eldebata/debate/VotingActivity.kt
@@ -3,10 +3,16 @@ package pl.elpassion.eldebata.debate
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import pl.elpassion.eldebata.R
 import pl.elpassion.eldebata.base.BaseActivity
+import pl.elpassion.eldebata.base.retrofit.applySchedulers
+import pl.elpassion.eldebata.debate.api.DebateApiProvider
+import pl.elpassion.eldebata.debate.api.DebateData
 
 class VotingActivity : BaseActivity() {
+
+    val api by lazy { DebateApiProvider.get() }
 
     companion object {
         fun start(context: Context) {
@@ -18,5 +24,13 @@ class VotingActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.voting_activity)
+        api.getDebateData().applySchedulers().subscribe(onGetDebateDataSuccess, onGetDebateDataFailure)
     }
+
+    val onGetDebateDataSuccess: (DebateData) -> Unit = {
+        val topic = findViewById(R.id.voting_activity_debate_topic) as TextView
+        topic.text = it.topic
+    }
+
+    val onGetDebateDataFailure: (Throwable) -> Unit = {}
 }
