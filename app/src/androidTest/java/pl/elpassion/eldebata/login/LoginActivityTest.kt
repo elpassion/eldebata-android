1b661916915d749059be75fffabe43fbf79b6974
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-04-25 13:27:55 +0200

    remove redundant files

--- app/src/androidTest/java/pl/elpassion/eldebata/login/LoginActivityTest.kt
+++ app/src/androidTest/java/pl/elpassion/eldebata/login/LoginActivityTest.kt
@@ -5,14 +5,13 @@ import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers
 import org.junit.Rule
 import org.junit.Test
-import pl.elpassion.eldebata.login.LoginActivity
 import pl.elpassion.eldebata.common.rule
 
 class LoginActivityTest {
 
     @JvmField @Rule
     val rule = rule<LoginActivity>()
-    
+
     @Test
     fun initTest() {
         Espresso.onView(ViewMatchers.withText("ElDebata!")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
