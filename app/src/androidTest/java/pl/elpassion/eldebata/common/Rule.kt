1c99a3e9e77040ab4e8a4cfab285c82aa72cef4d
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-04-25 16:22:55 +0200

    Added additional rule.

--- app/src/androidTest/java/pl/elpassion/eldebata/common/Rule.kt
+++ app/src/androidTest/java/pl/elpassion/eldebata/common/Rule.kt
@@ -7,3 +7,11 @@ import android.support.test.rule.ActivityTestRule
 inline fun <reified T : Activity> rule(): ActivityTestRule<T> {
     return object : ActivityTestRule<T>(T::class.java) {}
 }
+
+inline fun <reified T : Activity> rule(crossinline beforeActivityFunction: () -> Unit): ActivityTestRule<T> {
+    return object : ActivityTestRule<T>(T::class.java) {
+        override fun beforeActivityLaunched() {
+            beforeActivityFunction()
+        }
+    }
+}
